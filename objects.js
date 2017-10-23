306a8a61d8352f3a82404c2f4a1a0f8e3ef87ca1
Karen Lowe <k.szameit@mac.com>
2017-10-23 11:14:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(obj, key, value) {
   return obj
 }
 
-function removeFromPlaylist(obj, key) {
-  delete obj.key
-  return obj
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
 }
